054c0be4f69fd464c97cf647deeb25dc5bd8092a
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-07-23 10:19:57 +0000

    Variables config done

---
+++ 01_Basic/01_Variabels.js
@@ -0,0 +1,12 @@
+const accId=119876;
+let accType="Saving";
+var accEmail="test@123gmail.com";
+accCity="Delhi";
+
+// accId=5678; not allowed the in const case
+
+let accPassword;
+
+console.log(accId);
+
+console.table([accId,accType,accEmail,accCity,accPassword]);
