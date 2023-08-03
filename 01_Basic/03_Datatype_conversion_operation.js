8652b98da4301f4fc722ecb5caf79185180c947c
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-08-03 12:36:37 +0000

    Datatypes conversion

---
+++ 01_Basic/03_Datatype_conversion_operation.js
@@ -0,0 +1,28 @@
+let score="33asq";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//  "33asq" => NaN
+// true => 1 or false => 0
+
+let isLoggedIn="hi";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "hi" => true
+
+
+let someNumber=55;
+
+let stringSomeNumber=String(someNumber);
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
