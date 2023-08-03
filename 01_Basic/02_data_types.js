aa8cdcc1229a5cf2bca5c22b7dd23deae94cd9ed
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-08-03 12:34:39 +0000

    DataTypes Conversion

---
+++ 01_Basic/02_data_types.js
@@ -0,0 +1,8 @@
+"use strict" // treat all code as new JS Version
+
+// alert(8+1);  alert are use in browswe not work in node.
+
+console.log(6+
+    3);  //  this is not good practice. maintained good readability in the code
+
+console.log("hii this Vimal")
