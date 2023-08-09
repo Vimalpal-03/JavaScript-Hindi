22500495af2d356db77cbb6499e2405cc2cc5dd9
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-08-09 12:17:11 +0000

    04 Operations in JS

---
+++ 01_Basic/04_Operations.js
@@ -0,0 +1,23 @@
+let value=3
+let negValue=-value;
+console.log(negValue);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(2+2+"1");
+console.log(true);
+console.log(+true);
+
+
+// postfix increment(++)
+let x=3;
+const y=x++;
+console.log(`x:${x},y:${y}`);
+
+
+// prefic increment
+
+let a=6;
+const b=++a;
+console.log(`a:${a},b:${b}`);
