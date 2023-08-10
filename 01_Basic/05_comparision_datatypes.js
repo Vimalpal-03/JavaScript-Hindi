6cbd8ab588d9540090476160a59153af52581512
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-08-10 18:53:34 +0000

    Comparision Datatype

---
+++ 01_Basic/05_comparision_datatypes.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+
+// console.log("2">=1);
+// console.log("04">1);
+
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+
+// triple equal === to strict mode
+console.log(3===1);
+console.log("3"===1);
+console.log(NaN===1);
+
+
+
+
+
+
