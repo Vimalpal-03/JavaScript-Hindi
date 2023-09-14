a47a505d53659dbc6de6c073c234d06ff96c5454
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-09-14 14:27:10 +0000

    Numbers in JS

---
+++ 01_Basic/08_numbers.js
@@ -0,0 +1,13 @@
+const balance=3000;
+
+const score=new Number(100);
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const value=253.6556;
+console.log(value.toPrecision(4));
+
+const hundreds=10000000;
+console.log(hundreds.toLocaleString('en-In'));
