7aa632448f670ab12c77feec4a5ac4abadc8ab44
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-09-18 08:45:30 +0000

    Math methods in JS

---
+++ 01_Basic/09_Maths.js
@@ -0,0 +1,18 @@
+// console.log(Math);
+
+// console.log(Math.abs(9));
+// console.log(Math.round(88.49));
+
+// console.log(Math.ceil(55.1));
+// console.log(Math.floor(33.88));
+// console.log(Math.min(3,4,5,2));
+// console.log(Math.max(4,5,6,7));
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=1;
+const max=6;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
