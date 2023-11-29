a3433bd9d04a0d8c99c80508452e02f939f3d2d3
Vimalpal-03 <121486392+Vimalpal-03@users.noreply.github.com>
2023-11-29 14:41:12 +0000

    obj de-structure & JSON API

---
+++ 02_Basic/05_obj-destructure_json-api.js
@@ -0,0 +1,17 @@
+const course={
+    courseName:"Javascript",
+    price:"999",
+    courseInstructure:"Vimal"
+}
+// console.log(course.courseInstructure);
+const {courseName}=course
+console.log(courseName);
+
+// JSON-api
+[
+    {
+        name:"vimal",
+        courseName:"Javascript Hindi",
+        courseFee:"free"
+    }
+]
