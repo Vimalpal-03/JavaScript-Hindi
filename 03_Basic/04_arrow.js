95d49cecbf8a19604e28c4ffa7e55f2dbce8b3e4
Vimal Pal <121486392+Vimalpal-03@users.noreply.github.com>
2026-09-25 18:35:45 +0000

    arrow function review

--- 03_Basic/04_arrow.js
+++ 03_Basic/04_arrow.js
@@ -4,6 +4,7 @@ const user = {
 
     loggedMessage: function () {
         console.log(` hii... ${this.userName}, Welcome to website`);
+        // 'this' current context ko refer karta hai
         // console.log(this);
     }
 
@@ -13,11 +14,20 @@ const user = {
 //  user.loggedMessage()
 
 // console.log(this);
-
+/*
+In the above consonle that ' this ' give a empty object in node environment 
+because there is no context in global but in browser it gives a window browser object
+*/
+ 
+function code(){
+    // console.log(this)
+}
+code()
 
 const cod = function () {
     let name = "Vimal"
-    // console.log(this.name);
+    // console.log(this.name); 
+    // undefined because this is working in object not in function
 }
 cod();
 
@@ -31,11 +41,16 @@ cod();
 //++++++++++ Arrow function +++++++++++
 // const sumTwo = (num1, num2) => {
 //     return num1 + num2;
+/* explicit rerurn*/ 
 // }
 // console.log(sumTwo(3,4))
 
 // const sumTwo=(num1,num2)=>(num1+num2);
-const sumTwo=(num1,num2)=>({userName:"vayu"});
+/* implicit return me ham return word use nhi karte hai bas
+me print kr dete hai small code ko
+*/
+const sumTwo = (num1, num2) => ({ userName: "vayu" });
 
 console.log(sumTwo(3,4))
 
+ 
